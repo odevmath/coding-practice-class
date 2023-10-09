0a27b04262566ddf2a61b6d4d920287b9a081813
omathsantos <matheusraimundosantos@gmail.com>
2023-10-09 17:53:22 -0300

    Criei um array para as classes de heróis e comecei a criar a classe.

---
+++ coding-pratice-class copy.js
@@ -0,0 +1,12 @@
+let heroClass = [
+  ["guerreiro", "espada"],
+  ["mago", "magia"],
+  ["monge", "artes marciais"],
+  ["ninja","shuriken"]
+]
+
+class hero{
+  constructor(nome, idade, heroClass)
+  this.
+}
+
